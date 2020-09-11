328991699e1c7e3666a5f9f86fcf13aa7b9267c4
Alex Goodwin <hagoodwi@gmail.com>
2020-09-11 10:36:12 -0700

    add api call

--- currencyExchanger/src/currencyExchange.js
+++ currencyExchanger/src/currencyExchange.js
@@ -1,17 +1,19 @@
 export default class CurrencyExchange {  
-  static getExchange(city) {
+  static getExchange() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
+
+    
   }
 }
--- currencyExchanger/src/main.js
+++ currencyExchanger/src/main.js
@@ -2,7 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './currencyExchange';
 
 $(document).ready(function() {
-  
+  $('#currencyExchange').click(function(){
+    let exchange = $('#currency').val();
+    let country = $('#country').val();
+    $('#formOne').val("");
+    $('#currency').val("");
+    let promise = CurrencyExchange.getExchange(country, exchange);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('.showExchange').text(`The exchange rate of  in ${country} is ${body.conversion_rates.EGP}%`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error}`);
+    });
+
+  });
 });
